16a37490498091116d2b972e41ca20ee79243e5c
Shaan Amin <shaan.amin@igt.com>
2018-11-02 15:35:33 -0700

    Initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://shaanamin13:bears28@ds249583.mlab.com:49583/devconnector2'
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+//@route GET api/profile/test
+//@desc Tests Profile route
+//@access Public
+router.get('/test', (req, res) => res.json({
+    msg: "Profile Works"
+}));
+
+module.exports = router;

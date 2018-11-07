424cae9937a6f4617f072992725660ff51f560ea
shaanamin13 <aminshaan2810@gmail.com>
2018-11-06 23:31:10 -0800

    added register. but debugging

--- models/User.js
+++ models/User.js
@@ -1,30 +1,27 @@
-const mongoose = require('mongoos');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 
 //Create Schema
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model('users', UserSchema);
+module.exports = User = mongoose.model("users", UserSchema);

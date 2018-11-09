87f205b9465be305b01310d2a820eb0846fe2809
Shaan Amin <shaan.amin@igt.com>
2018-11-08 16:21:44 -0800

    Profile Schema complete

--- models/Profile.js
+++ models/Profile.js
@@ -61,7 +61,58 @@ const ProfileSchema = new Schema({
     description: {
       type: String
     }
-  }]
+  }],
+
+  education: [{
+    school: {
+      type: String,
+      required: true
+    },
+    degree: {
+      type: String,
+      required: true
+    },
+    fieldofstudy: {
+      type: String,
+      required: true
+    },
+    from: {
+      type: Date,
+      required: true
+    },
+    to: {
+      type: Date
+    },
+    current: {
+      type: Boolean,
+      default: false
+    },
+    description: {
+      type: String
+    }
+  }],
+
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model("users", UserSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);

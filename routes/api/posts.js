361930acdf5175ad2a54edd8a187d9889f6e93ad
shaanamin13 <aminshaan2810@gmail.com>
2018-11-10 22:15:21 -0800

    Post request for Posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//Post Model
+const Post = require("../../models/Posts");
+
+// Validation
+const validatePostInput = require("../../validation/post");
+
 //@route GET api/posts/test
 //@desc Tests post route
 //@access Public
@@ -10,6 +16,29 @@ router.get("/test", (req, res) =>
   res.json({
     msg: "Posts Works"
   })
+);
+
+//@route Posts api/posts
+//@desc Create Post
+//@access private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //Check validation
+    if (!isValid) {
+      // if any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
 );
 
 module.exports = router;

f3033d0fbedb557581fae6a2689f6a7f39ef99cd
Shaan Amin <shaan.amin@igt.com>
2018-11-24 11:21:32 -0500

    created textfieldgroup template for faster form creation

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utls/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
@@ -21,6 +21,18 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+
+  if (decoded.exp < currentTime) {
+    //Logout user
+    store.dispatch(logoutUser());
+
+    //Todo: Clear current Profile
+    //Redirect to login
+    window.location.href = "/login";
+  }
 }
 class App extends Component {
   render() {
---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  profile: null,
+  profiles: null,
+  loading: false
+};
+
+export default function(state = initialState, actions) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}

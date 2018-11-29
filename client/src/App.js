208050f3105e047721039818c174025426d1c376
shaanamin13 <aminshaan2810@gmail.com>
2018-11-28 19:58:43 -0800

    Education form added

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 import "./App.css";
 
 //Check for token
@@ -77,6 +78,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -52,6 +52,19 @@ export const addExperience = (expData, history) => dispatch => {
     );
 };
 
+//Add education
+export const addEducation = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can Not be undone!")) {

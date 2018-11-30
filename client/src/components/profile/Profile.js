db19020cd2f1d3e3f418e42e9ca13519a89c0622
shaanamin13 <aminshaan2810@gmail.com>
2018-11-29 20:53:04 -0800

    Adding Profile Components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+
+import { getProfileByHandle } from "../../actions/profileActions";
+
 import { Link } from "react-router-dom";
 import ProfileHeader from "./ProfileHeader";
 import ProfileCreds from "./ProfileCreds";
@@ -8,23 +11,57 @@ import ProfileGithub from "./ProfileGithub";
 import ProfileAbout from "./ProfileAbout";
 import Spinner from "../common/Spinner";
 class Profile extends Component {
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
   static propTypes = {
     prop: PropTypes
   };
 
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div className="row">
+          <div className="col-md-6">
+            <Link to="/profiles" className="btn btn-light mb-3 float-left">
+              Back to Profiles
+            </Link>
+          </div>
+          <div className="col-md-6" />
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1>TODO: Profile </h1>
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 Profile.propTypes = {
+  getProfileByHandle: PropTypes.func.isRequired,
   profile: PropTypes.object.isRequired
 };
-const mapStateToProps = state => {
-  Profile = state.profile;
-};
-export default connect(mapStateToProps)(Profile);
+const mapStateToProps = state => ({
+  profile: state.profile
+});
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);

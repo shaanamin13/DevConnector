895a6b7ae0c32a71e2c19e7300cef1174871c86e
Shaan Amin <shaan.amin@igt.com>
2018-12-10 12:16:18 -0800

    Abilty to make posts for user

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,3 +1,4 @@
+import { ADD_POST } from "../actions/types";
 const initialState = {
   posts: [],
   post: {},
@@ -6,6 +7,11 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
     default:
       return state;
   }
